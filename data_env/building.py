d7c665b8a9ed18f4a38fb9afdc73c03f2aa156d7
Coline_Piera <132573147+CocoPiera@users.noreply.github.com>
2024-05-14 15:50:08 +0200

    Implementation Planification

--- data_env/building.py
+++ data_env/building.py
@@ -1,10 +1,14 @@
-class Building:
-    def __init__(self, id_building, lst_infras ) :
-        self.id_building = id_building
-        self.lst_infras = lst_infras
+class Building : 
+	def __init__ (self, id_building, list_infras) : 
+		self.id_building = id_building 
+		self.list_infras = list_infras 
 
-    def get_building_difficulty(self):
-        return sum(self.lst_infras)
-    
-    def _lt_ (self, other_building) :
-        return self.get_building_difficulty() < other_building.get_building_difficulty()
+	def get_building_difficulty (self) : 
+		return sum(self.list_infras)
+
+	def __lt__ (self, other_building) :
+		return self.get_building_difficulty() < other_building.get_building_difficulty()
+
+	def __repr__(self):
+		return self.id_building
+  
